07e7855950d4c77bf5a160ab295ca90513a7b02a
Sono Pathak <SP9991827@Hello-Noida.com>
2024-07-27 15:47:05 +0530

    made requirments file and sqllite db

--- invoice/settings.py
+++ invoice/settings.py
@@ -76,31 +76,31 @@ WSGI_APPLICATION = 'invoice.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.mysql",
-    #     "NAME": "invoice",
-    #     "USER": "root",
-    #     "PASSWORD": "root",
-    #     "HOST": "127.0.0.1",
-    #     "PORT": "3306",
-    # }
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "invoice",
-        "USER": "root",
-        "PASSWORD": "root@12345678",
-        "HOST": "127.0.0.1",
-        "PORT": "3306",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     # "default": {
+#     #     "ENGINE": "django.db.backends.mysql",
+#     #     "NAME": "invoice",
+#     #     "USER": "root",
+#     #     "PASSWORD": "root",
+#     #     "HOST": "127.0.0.1",
+#     #     "PORT": "3306",
+#     # }
+#     # "default": {
+#     #     "ENGINE": "django.db.backends.mysql",
+#     #     "NAME": "invoice",
+#     #     "USER": "root",
+#     #     "PASSWORD": "root@12345678",
+#     #     "HOST": "127.0.0.1",
+#     #     "PORT": "3306",
+#     # }
+# }
 
 
 # Password validation
